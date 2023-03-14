13a8deacd7d8cc4303a61127a841113ab435f965
ahmedkhaeld <ahmedkhaledmohamed9@gmail.com>
2023-03-14 14:18:38 +0200

    Update routing to support actions from array

--- src/app/Router.php
+++ src/app/Router.php
@@ -9,18 +9,18 @@ use App\Exceptions\RouteNotFoundException;
 class Router
 {
     /**
-     * @var array $routes
+     * @param array $routes
      * the routes and their actions
      */
     private array $routes;
 
     /**
+     * @param callable|array $action
+     * @param string $route
      * @return self $this the current instance of the router
      * register a route and its action to the router
-     * @var string $route
-     * @var callable $action
      */
-    public function register(string $route, callable $action): self
+    public function register(string $route, callable|array $action): self
     {
         $this->routes[$route] = $action;
 
@@ -31,7 +31,7 @@ class Router
     /**
      * @return mixed the result of the action
      * @throws RouteNotFoundException
-     * @var string $requestUri
+     * @param string $requestUri
      * resolve the route and return the result of the action
      */
     public function resolve(string $requestUri): mixed
@@ -42,7 +42,26 @@ class Router
         if (!$action) {
             throw new  RouteNotFoundException();
         }
+        //check if the action is callable
+        if (is_callable($action)) {
+            return call_user_func($action);
+        }
 
-        return call_user_func($action);
+        //check if the action is an array
+        if (is_array($action)) {
+            //destructuring the array
+            [$controller, $method] = $action;
+            //check if controller exists
+            if (class_exists($controller)){
+                $controller = new $controller();
+                //check if method exists
+                if (method_exists($controller, $method)) {
+                    return call_user_func([$controller, $method]);
+                }
+            }
+        }
+        throw new RouteNotFoundException();
+
+
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -2,18 +2,19 @@
 
 declare(strict_types=1);
 
+use App\Controllers\HomeController;
+use App\Controllers\InvoiceController;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 
 $router = new App\Router();
 
 
-$router->register('/', function () {
-    echo 'Home';
-});
+$router->register('/', [HomeController::class, 'index'])
+        ->register('/invoices', [InvoiceController::class, 'index'])
+        ->register('/invoices/create', [InvoiceController::class, 'create'])
+;
 
-$router->register('/invoices', function () {
-    echo 'Invoices';
-});
 
 echo $router->resolve($_SERVER['REQUEST_URI']);
